32dd2f579a951cc3862f53416bc4c2bf8824173d
Paul Stone <pmstone247@gmail.com>
2013-09-13 20:31:52 -0600

    changed gemfile for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Suggestotron::Application.routes.draw do
+  resources :topics
+
+  root 'topics#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
